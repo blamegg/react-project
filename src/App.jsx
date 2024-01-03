64e16dab6b8eea39efbb675404e576b0b899e427
shivang <shivang.ramola.hp@gmail.com>
2024-01-03 22:14:12 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Sample from "./components/Sample";
+
+const App = () => {
+  return (
+    <>
+      <Sample />
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/Sample.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Sample = () => {
+  return (
+    <div className="text-4xl font-medium">Sample component is working</div>
+  );
+};
+
+export default Sample;
